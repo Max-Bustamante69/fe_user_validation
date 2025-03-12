c3001a6e6331a77c2603ed05593d43b33a14db94
vvhornung <vvhornung@eafit.edu.co>
2025-03-12 15:04:57 -0500

    making the form a component

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const documentTypeOptions = [
+    { value: "cc", label: "Cedula de Ciudadania" },
+    { value: "ti", label: "Tarjeta de Identidad" },
+    { value: "pp", label: "Pasaporte" },
+    { value: "dl", label: "Licencia de Conduccion" },
+]
