817b061bb7eb3c7af744d0ab503b40a049cdb9b4
vale <vvhornung@eafit.edu.co>
2025-03-12 18:45:50 -0500

    Created the Pagui layout page

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,33 @@
+
+import "../styles/tailwind.css";
+import logo from "../assets/pagui-logo.png";
+import vector1 from "../assets/pagui-vector-1.svg";
+import vector2 from "../assets/pagui-vector-2.svg";
+import Card from "../components/Card";
+import { Outlet } from "react-router";
+
+function Layout() {
+    return (
+      <div className="h-screen w-full flex items-center justify-center bg-white overflow-hidden relative">
+        <img
+          src={vector1}
+          alt="Pagui Vector"
+          className="absolute -top-24 z-0 left-0 w-1/5"
+        />
+        <img
+          src={vector2}
+          alt="Pagui Vector"
+          className="absolute -bottom-8 z-0 right-0 w-1/3"
+        />
+
+        <Card className="w-fit py-12 px-32 z-10  bg-white/80 backdrop-blur-xl shadow-2xl shadow-drop-shadow rounded-xl ">
+          <div className="flex flex-col gap-8 items-start w-full">
+            <img className="w-24" src={logo} alt="Pagui Logo" />
+            <Outlet />
+          </div>
+        </Card>
+      </div>
+    );
+}
+
+export default Layout;
