04544a00e839bbb93fc63d24dee9b91b6801e3f8
CeyniPBH <cpbeltranh@eafit.edu.co>
2025-03-13 11:54:59 -0500

    Added a is mobile util for beakpoint management

--- src/pages/Mobile/LayoutMobile.jsx
+++ src/pages/Mobile/LayoutMobile.jsx
@@ -5,15 +5,15 @@ import logo from '@assets/pagui-logo.png';
 
 const LayoutMobile = () => {
   return (
-    <div className="h-screen w-full flex flex-col items-center  bg-white overflow-hidden relative p-8">
+    <div className="h-screen w-full flex flex-col items-center justify-start gap-4  bg-white overflow-hidden relative p-8">
      
-        <Link className='flex w-full justify-start' to="/" aria-label="Go to Home Page">
+        <Link className='flex w-full justify-start sticky top-2 left-12' to="/" aria-label="Go to Home Page">
           <img className="w-16" src={logo} alt="Pagui Logo" />
         </Link>
    
-      <div className="flex flex-col items-center justify-around w-full p-4">
+      
         <Outlet />
-      </div>
+   
     </div>
   );
 };
