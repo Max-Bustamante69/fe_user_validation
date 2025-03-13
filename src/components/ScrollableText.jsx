15ffd60db06b4defb05daa1c1ccfc1a9d0969908
CeyniPBH <cpbeltranh@eafit.edu.co>
2025-03-13 11:57:07 -0500

    Updated styles for responsive terms and conditions forms

--- src/components/ScrollableText.jsx
+++ src/components/ScrollableText.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 
 
-const ScrollableText = ({ text }) => {
+const ScrollableText = ({ text, className }) => {
   return (
-    <div className="transform  w-full max-w-lg   bg-transparent ">
-      <div className="scrollbar h-48 overflow-y-auto p-4 scrollbar-thin ">
+    <div className={`string text ${className} string text`}>
+      <div className="scrollbar h-fit lg:h-48 overflow-hidden  overflow-y-scroll max-h-full p-4 scrollbar-thin ">
         <p className="whitespace-pre-line text-placeholder-text leading-relaxed">
           {text}
         </p>
