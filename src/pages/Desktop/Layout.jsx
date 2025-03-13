9ee47d6d354a886548fa7265efd5d5d72dc0ac70
vvhornung <vvhornung@eafit.edu.co>
2025-03-13 08:02:56 -0500

    REMOVED LAZY LOADING FOR LAYOUT

--- src/pages/Desktop/Layout.jsx
+++ src/pages/Desktop/Layout.jsx
@@ -14,20 +14,18 @@ function Layout() {
         src={vector1}
         alt="Pagui Vector"
         className="absolute -top-24 z-0 left-0 w-1/5"
-        loading="lazy"
       />
 
       <img
         src={vector2}
         alt="Pagui Vector"
         className="absolute -bottom-8 z-0 right-0 w-1/3"
-        loading="lazy"
       />
 
       <Card className="w-2/3 min-h-4/5 py-12 justify-center z-10  bg-white/80 backdrop-blur-xl shadow-2xl shadow-drop-shadow rounded-xl ">
         <div className="flex flex-col gap-8 items-start  w-full">
           <Link to="/" aria-label="Go to Home Page">
-            <img className="w-24" src={logo} alt="Pagui Logo" loading="lazy" />
+            <img className="w-24" src={logo} alt="Pagui Logo"  />
           </Link>
           <div className="flex justify-around items-space  w-full">
             <Outlet />
