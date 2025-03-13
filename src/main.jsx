c8e9bd3d960eb2b7de306c3c5444b6de15d2b9f4
vale <vvhornung@eafit.edu.co>
2025-03-12 19:32:55 -0500

    Imported the animated routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
 
-import Layout from './pages/Layout'
-import Home from './pages/Home'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import AnimatedRoutes from './AnimatedRoutes'
+
 
 
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />} >
-        <Route index element={<Home/>} />
-      </Route>
-      
-      
-    
-    </Routes>
+    <AnimatedRoutes />
   </BrowserRouter>
 )
