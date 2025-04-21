2a43f94b3ba3032fea6a368ace5eaa7c68f0c5e5
Max <mbustamang@eafit.edu.co>
2025-04-20 21:45:47 -0500

    wrapped the provider around the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import { AuthProvider } from './contexts/AuthContext'
 import AnimatedRoutes from './AnimatedRoutes'
 
 
@@ -8,7 +9,10 @@ import AnimatedRoutes from './AnimatedRoutes'
 
 
 createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    
   <BrowserRouter>
     <AnimatedRoutes />
   </BrowserRouter>
+  </AuthProvider>
 )
