ab418fd9e1113a0cc4aad0afefa712ed316f7b24
Max <mbustamang@eafit.edu.co>
2025-04-23 15:28:17 -0500

    Fixed content overlay in the front end

--- src/pages/Mobile/LayoutMobile.jsx
+++ src/pages/Mobile/LayoutMobile.jsx
@@ -5,8 +5,8 @@ import logo from '@assets/pagui-logo.png';
 
 const LayoutMobile = () => {
   return (
-    <div className="h-dvh w-full flex flex-col items-center justify-start gap-4  bg-white overflow-hidden relative p-4">
-      <Link className='flex w-full justify-start sticky top-2 left-12' to="/" aria-label="Go to Home Page">
+    <div className="h-dvh w-full flex flex-col items-center justify-start gap-4  bg-white overflow-x-hidden overflow-auto relative pb-4 px-4">
+      <Link className='flex w-dvw bg-white pl-4 py-4 shadow justify-start sticky top-2 left-12' to="/" aria-label="Go to Home Page">
         <img className="w-16" src={logo} alt="Pagui Logo" width="64" height="64" />
       </Link>
       <Outlet />
