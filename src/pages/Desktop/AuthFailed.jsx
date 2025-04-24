168d3a1d1c36303980a276c27bf50a637dc2fc2c
Max <mbustamang@eafit.edu.co>
2025-04-23 21:34:22 -0500

    updated icon for error page

--- src/pages/Desktop/AuthFailed.jsx
+++ src/pages/Desktop/AuthFailed.jsx
@@ -1,37 +1,54 @@
 import React from 'react'
 import Button from '@components/Button'
 import { MdNavigateBefore } from 'react-icons/md'
-import { IoCheckmarkOutline } from 'react-icons/io5'
+import { FaHouse } from "react-icons/fa6";
 import { Link } from 'react-router'
 import { isMobile } from '@/lib/utils/isMobile'
+
 
 
 function AuthFailed() {
   return (
     <>
-    <div className=" flex flex-col gap-2 items-center text-center">
-        <h1 className="text-2xl lg:text-3xl text-primary font-bold">ALGO SALIO MAL...</h1>
+      <div className=" flex flex-col gap-2 items-center text-center">
+        <h1 className="text-2xl lg:text-3xl text-primary font-bold">
+          ALGO SALIO MAL...
+        </h1>
         <h2 className="text-xl lg:text-2xl text-secondary-text font-light">
           No eres tu? contacta el soporte tecnico de pagui
         </h2>
       </div>
       <div className="flex w-full justify-between items-center absolute lg:relative bottom-0">
         <Link to={"/didit"} aria-label="Retry Authentication">
-          <Button variant={"rounded"} size={isMobile() ? "roundMd" : "roundLg"} aria-label="Back">
+          <Button
+            variant={"rounded"}
+            size={isMobile() ? "roundMd" : "roundLg"}
+            aria-label="Back"
+          >
             <MdNavigateBefore className="w-8 h-8" />
           </Button>
         </Link>
-        <Link to={"https://pagui.co/"} target="_blank" aria-label="Contact Technical Support">
-          <Button variant={"underline"} aria-label="Soporte">SOPORTE TECNICO</Button>
+        <Link
+          to={"https://pagui.co/"}
+          target="_blank"
+          aria-label="Contact Technical Support"
+        >
+          <Button variant={"underline"} aria-label="Soporte">
+            SOPORTE TECNICO
+          </Button>
         </Link>
         <Link to={"/"} aria-label="Go to Home Page">
-          <Button variant={"rounded"} size={isMobile() ? "roundMd" : "roundLg"} aria-label="Next">
-            <IoCheckmarkOutline className="w-8 h-8" />
+          <Button
+            variant={"rounded"}
+            size={isMobile() ? "roundMd" : "roundLg"}
+            aria-label="Next"
+          >
+            <FaHouse className="w-8 h-8" />
           </Button>
         </Link>
       </div>
     </>
-  )
+  );
 }
 
 export default AuthFailed
