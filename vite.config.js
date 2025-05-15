22a5b594a7b403f3283f8e6a7507a346d8411e80
val <vvhornung@eafit.edu.co>
2025-05-14 23:14:39 -0500

    deployed the robots.txt for the crawlers

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
-import viteCompression from 'vite-plugin-compression';
+import viteCompression from "vite-plugin-compression";
 
 // Create __dirname equivalent for ESM
 const __filename = fileURLToPath(import.meta.url);
@@ -22,6 +22,15 @@ export default defineConfig({
       "@pages": resolve(__dirname, "./src/pages"),
       "@assets": resolve(__dirname, "./src/assets"),
       "@styles": resolve(__dirname, "./src/styles"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        robots: resolve(__dirname, "robots.txt"),
+        sitemap: resolve(__dirname, "public/sitemap.xml"),
+      },
     },
   },
 });
