bde7d64eccf20670274d3c19a1cecb42a9523ab4
val <vvhornung@eafit.edu.co>
2025-05-14 23:03:01 -0500

    Styling change for the select field

--- src/components/SelectField.jsx
+++ src/components/SelectField.jsx
@@ -6,18 +6,14 @@ const SelectField = ({ label, options, value, ...props }) => {
       <label className="font-semibold text-primary ">{label}</label>
       <select
         value={value}
-        className="w-full font-semibold appearance-none p-2 mt-1 border border-gray-300 rounded-md bg-white text-secondary-text focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary transition-all duration-200 disabled:text-neutral-600 disabled:cursor-not-allowed disabled:bg-gray-100"
+        className="w-full font-semibold appearance-none p-2 mt-1 border border-gray-300 rounded-md bg-white text-secondary-text  focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary transition-all duration-200 disabled:text-neutral-600 disabled:cursor-not-allowed disabled:bg-gray-100"
         {...props}
       >
         <option value="" disabled hidden>
           Selecciona una opción
         </option>
         {options?.map((option, index) => (
-          <option
-            key={index}
-            value={option.value}
-            className="text-placeholder-text"
-          >
+          <option key={index} value={option.value}>
             {option.label}
           </option>
         ))}
